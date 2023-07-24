3110ff552731650a67018eda489132438bb646dc
miccou <michael.cousins@outlook.com>
2023-07-24 21:16:46 +1000

    add theme colours to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,35 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/app/**/*.ts", "./src/app/**/*.html"],
   theme: {
+    colors: {
+      'cutty-sark': {
+        '50': '#f4f7f7',
+        '100': '#e3e9ea',
+        '200': '#cad5d7',
+        '300': '#a4b7bc',
+        '400': '#789198',
+        '500': '#5d767d',
+        '600': '#556a72',
+        '700': '#455359',
+        '800': '#3d484d',
+        '900': '#363e43',
+        '950': '#21272b',
+      },
+      'aquamarine': {
+        '50': '#e9fff7',
+        '100': '#caffea',
+        '200': '#9bffdb',
+        '300': '#73fbd3',
+        '400': '#1becb5',
+        '500': '#00d4a0',
+        '600': '#00ad84',
+        '700': '#008b6d',
+        '800': '#006d57',
+        '900': '#005949',
+        '950': '#00332b',
+      },
+
+    },
     extend: {
       fontFamily: {
         sans: ["Varela Round", ...defaultTheme.fontFamily.sans],
