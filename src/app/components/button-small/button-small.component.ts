27da1ad862c8bbd1039e3b7f9a2eaa8326f2296c
miccou <michael.cousins@outlook.com>
2023-07-29 14:06:51 +1000

    refactor buttons into common components

---
+++ src/app/components/button-small/button-small.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-button-small",
+  template: `
+    <div
+      class="m-auto flex h-8 w-20 cursor-pointer items-center justify-center gap-1 rounded-md border border-aquamarine-950 bg-gradient-to-br from-aquamarine-400 to-aquamarine-500 p-2 text-cutty-sark-950 hover:from-aquamarine-300 hover:to-aquamarine-400">
+      <ng-content></ng-content>
+    </div>
+  `,
+})
+export class ButtonSmallComponent {}
