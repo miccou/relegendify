b856d1820bedc9d3a649493ecdf1a619ddc194d1
miccou <michael.cousins@outlook.com>
2023-07-29 15:25:24 +1000

    add print button, fix some styles

--- src/app/components/button-medium/button-medium.component.ts
+++ src/app/components/button-medium/button-medium.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
   selector: "app-button-medium",
   template: `
     <div
-      class="flex flex-row items-center gap-2 rounded-md border border-aquamarine-950 bg-gradient-to-br from-aquamarine-400 to-aquamarine-500 p-2 text-cutty-sark-950 hover:from-aquamarine-300 hover:to-aquamarine-400">
+      class="flex cursor-pointer flex-row items-center gap-2 rounded-md border border-aquamarine-950 bg-gradient-to-br from-aquamarine-400 to-aquamarine-500 p-2 text-cutty-sark-950 hover:from-aquamarine-300 hover:to-aquamarine-400">
       <ng-content></ng-content>
     </div>
   `,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,4 +4,8 @@ import { Component } from "@angular/core";
   selector: "app-home",
   templateUrl: "./home.component.html",
 })
-export class HomeComponent {}
+export class HomeComponent {
+  print() {
+    window.print();
+  }
+}
